6b7a3995967e6e30eb43375869db0f5b545902d0
michal salaciak <michal.salaciak@securitas.com>
2020-09-21 16:18:15 +0200

    improve logs

--- scripts/kube-log.sh
+++ scripts/kube-log.sh
@@ -18,14 +18,15 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-echo "$PATTERN"
-echo "$CONTAINER"
-
 POD_NAMES=$(kubectl get pods --template '{{range .items}}{{.metadata.name}}{{";"}}{{end}}')
 IFS=';' read -ra POD_NAMES_ARRAY <<<"$POD_NAMES"
 
+echo "Searching for pods by the pattern: $PATTERN with container: $CONTAINER"
+
 for name in "${POD_NAMES_ARRAY[@]}"; do
     if [[ $name =~ "$PATTERN" ]]; then
+        echo "Logging from the pod: $name"
+
         osascript -e "tell app \"Terminal\"
             do script \"kubectl logs -f $name -c $CONTAINER\"
         end tell"
